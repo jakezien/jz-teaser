6c0e68a75e76ede6488473b77d787345a7881437
Jake Zien <jake@floored.com>
2020-03-05 16:50:35 -0500

    starting typography

--- src/components/postPreview.js
+++ src/components/postPreview.js
@@ -1,15 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+import { rhythm, scale } from "../utils/typography"
+import styled from "styled-components"
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
 
 const PostPreview = ({ post, className }) => {
   return (
-    <Link to={post.fields.slug} className={className}>
-      <div>
+    <div className={className}>
+      <StyledLink to={post.fields.slug}>
         <h2>{post.frontmatter.title}</h2>
         <caption>{post.frontmatter.category}</caption>
         <p>{post.excerpt}</p>
-      </div>
-    </Link>
+      </StyledLink>
+    </div>
   )
 }
 
