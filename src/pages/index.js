f20bf0e9da2e8cd986ba6ab1fc08d16cf6c3b462
Jake Zien <jake@floored.com>
2020-08-31 22:43:45 -0400

    testing styled components in about to see if it breaks the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ const Home = ({ data, location }) => {
   return (
     <Layout>
       <SEO title="Jake Zien" />
+      OKOKOK
       <Intro />
-      YAAA
       <Teaser posts={data.workPosts.edges} title="Work" linkTo="/work" linkText="All work"/>
       <Teaser posts={data.thingsPosts.edges} title="Things" linkTo="/things" linkText="All things" postsPerRow="4"/>
     </Layout>
