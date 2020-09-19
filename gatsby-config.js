e124c3f168e3c3065023290a795c98a97a012b01
Jake Zien <jake@floored.com>
2020-09-19 17:05:44 -0400

    trying to fix styles loading issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
