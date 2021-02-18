7e080737bdc6559a319af660e3dd4bb85d6477b1
Jake Zien <jake@floored.com>
2021-02-17 20:42:19 -0500

    myndy page work

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,12 +1,20 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 // input a collection of file nodes and return an array of {src, height, width}
-export function galleryArray(props, namesToSkip) {
+export function galleryArray(props, names, namesToSkip) {
   let array = [];
   for (let i in props.images) {
     if (namesToSkip && namesToSkip.includes(props.images[i].name))
       continue;
-    array.push(props.images[i].childImageSharp.original)
+    if (names) {
+      for (let j in names) {
+        if (props.images[i].name.includes(names[j])) {
+          array.push(props.images[i].childImageSharp.original);
+          continue;
+        }
+      }
+    }
+    else array.push(props.images[i].childImageSharp.original);  
   }
   return array;
 }
