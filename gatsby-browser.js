942ea1b9ce073e59febc2f99486192673a71cd63
jakezien <jakezien@gmail.com>
2021-03-02 08:16:18 -0500

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import "./static/fonts/fonts.css"
 import React from 'react';
-import TCensorWrapper from './src/components/TCensor'
+import TCensorWrapper from './src/components/tCensor'
 
 export const wrapPageElement = ({element, props}) => (
   <TCensorWrapper {...props}>{element}</TCensorWrapper>
