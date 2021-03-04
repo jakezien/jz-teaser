de8440c0acc55f39135030f17f36da9a8487df61
Jake Zien <jake@floored.com>
2021-03-04 08:59:27 -0500

    tcensor fix hopefully

--- src/components/tCensor.js
+++ src/components/tCensor.js
@@ -15,6 +15,7 @@ const TextCensor = () => {
       wrap: "span",
       wrapClass: 'censored',
       filterElements: (node) => {
+        if (!node || !node.parentNode) return
         if (node.parentNode.classList.contains('censored') || node.classList.contains('censored')) 
           return false
         else return true;
