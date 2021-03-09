36a0d6b474f6cd76f46f8b582925a696581840c0
Jake Zien <jake@floored.com>
2021-03-09 18:16:31 -0500

    google verification

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,6 @@ import TCensorWrapper from './src/components/tCensor'
 export const wrapPageElement = ({element, props}) => (
   <>
     <Helmet>
-      <meta name="google-site-verification" content="JriCc3UmjDhEpJeVf7c0HZiWpq2Ou9IWZAcPeD5VVzk" />
       <script async defer data-domain="jakezien.github.io/jz" src="https://plausible.io/js/plausible.js"></script>
     </Helmet>
     <TCensorWrapper {...props}>{element}</TCensorWrapper>
