dc3279ecf2d803789b608967d85d4720dde66246
Jake Zien <jake@floored.com>
2021-04-12 19:05:42 -0400

    remove plausible, add protegant

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,8 +7,8 @@ import TCensorWrapper from './src/components/tCensor'
 export const wrapPageElement = ({element, props}) => (
   <>
     <Helmet>
+      <script data-host="https://protegant.io" data-dnt="false" src="https://protegant.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
       <script async data-goatcounter="https://jakezien.goatcounter.com/count" src="//gc.zgo.at/count.js"></script>
-      <script async defer data-domain="jakezien.github.io/jz" src="https://plausible.io/js/plausible.js"></script>
     </Helmet>
     <TCensorWrapper {...props}>{element}</TCensorWrapper>
   </>
