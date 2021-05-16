a79839b3ce02c8cd65b25e1f04aaadf1a0d0678d
Jake Zien <jake@floored.com>
2021-05-15 22:32:25 -0700

    wy text and new analytics :\/

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,8 @@ export const wrapPageElement = ({element, props}) => (
     <Helmet>
       <script data-host="https://protegant.io" data-dnt="false" src="https://protegant.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
       <script async data-goatcounter="https://jakezien.goatcounter.com/count" src="//gc.zgo.at/count.js"></script>
+      <script async src="https://app.jitt.io/pixel/I5EkPtLtiykovzTw"></script>
+      <script src="https://privera.io/aap.js" pe-ua="UA-58831210-1" async></script>
     </Helmet>
     <TCensorWrapper {...props}>{element}</TCensorWrapper>
   </>
