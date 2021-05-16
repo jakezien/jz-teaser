e67819c4ef51a4aed27d2c64799f60c1272566fb
Jake Zien <jake@floored.com>
2021-05-15 23:03:55 -0700

    move analytics out of head?

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,12 +5,12 @@ import TCensorWrapper from './src/components/tCensor'
 
 export const wrapPageElement = ({element, props}) => (
   <>
-    <Helmet>
-      <script data-host="https://protegant.io" data-dnt="false" src="https://protegant.io/js/script.js" id="ZwSg9rf6GA" async defer></script>
-      <script async data-goatcounter="https://jakezien.goatcounter.com/count" src="//gc.zgo.at/count.js"></script>
-      <script async src="https://app.jitt.io/pixel/I5EkPtLtiykovzTw"></script>
-      <script src="https://privera.io/aap.js" pe-ua="UA-58831210-1" async></script>
-    </Helmet>
-    <TCensorWrapper {...props}>{element}</TCensorWrapper>
+      <TCensorWrapper {...props}>
+            {element}
+      </TCensorWrapper>
+      <script defer data-host="https://protegant.io" data-dnt="false" src="https://protegant.io/js/script.js" id="ZwSg9rf6GA"></script>
+      <script data-goatcounter="https://jakezien.goatcounter.com/count" src="//gc.zgo.at/count.js"></script>
+      <script src="https://app.jitt.io/pixel/I5EkPtLtiykovzTw"></script>
+      <script src="https://privera.io/aap.js" pe-ua="UA-58831210-1" ></script>
   </>
 )
