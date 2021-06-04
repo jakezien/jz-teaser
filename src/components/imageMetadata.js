dfdfe450b5e9a50978986731658df447ab6158f9
Jake Zien <jake@floored.com>
2021-06-03 23:59:31 -0700

    datetime fix

--- src/components/imageMetadata.js
+++ src/components/imageMetadata.js
@@ -1,4 +1,5 @@
 import React from "react"
+import moment from 'moment'
 import styled from "styled-components"
 import { rhythm } from "../utils/typography"
 
@@ -24,13 +25,12 @@ const StyledDiv = styled.div`
 const ImageMetadata = ({image}) => {
   let make = image.fields.exif.image.Make
   let model = image.fields.exif.image.Model
-  // let date = new Date(image.fields.exif.iptc.date_time).toLocaleDateString([], {weekday:'long', year:'numeric', month:'long', day:'numeric', hour:'numeric', minute:'numeric'})
-  let localeDate = new Date(image.fields.exif.exif.DateTimeOriginal).toLocaleDateString([], {weekday:'long', year:'numeric', month:'long', day:'numeric', hour:'numeric', minute:'numeric'})
-  let date = new Date(image.fields.exif.exif.DateTimeOriginal).toDateString()
-  let time = new Date(image.fields.exif.exif.DateTimeOriginal).toTimeString()
+  let utcDate = image.fields.exif.exif.DateTimeOriginal
+  let displayDate = moment.utc(utcDate).format('MMM D, YYYY h:mm A')
+  
   return (
     <StyledSpan>
-      {make} {model}&nbsp;&nbsp;|&nbsp;&nbsp;{date} {time} {localeDate}
+      {make} {model}&nbsp;&nbsp;|&nbsp;&nbsp;{displayDate}
     </StyledSpan>
   )
 }
