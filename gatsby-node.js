6bf3afc3cffe4a12094cee2477b7039859d3db37
Jake Zien <jake@floored.com>
2021-06-14 00:48:47 -0400

    gatsby-node fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,104 +7,12 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const { createPage } = actions
 
-  // ———— WORK PAGES ———— //
-  const workPostTemplate = path.resolve(`./src/templates/work-post.js`)
-  const workResult = await graphql(
-    `
-      {
-        allMdx(
-          filter: {fileAbsolutePath: {regex: "/content/work/"}}
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                type
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-
-  if (workResult.errors) {
-    throw workResult.errors
-  }
-
-  // Create work pages
-  const workPages = workResult.data.allMdx.edges
-  workPages.forEach((post, index) => {
-    const previous = index === workPages.length - 1 ? null : workPages[index + 1].node
-    const next = index === 0 ? null : workPages[index - 1].node
-
-    createPage({
-      path: post.node.fields.slug,
-      component: workPostTemplate,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
-  })
-
-
-
-
-  // ———— BLOG PAGES ———— //
-  const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
-  const blogResult = await graphql(
-    `
-      {
-        allMdx(
-          filter: {fileAbsolutePath: {regex: "/content/blog/"}}
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-
-  if (blogResult.errors) {
-    throw blogResult.errors
-  }
-
-  // Create blog posts pages.
-  const posts = blogResult.data.allMdx.edges
-
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPostTemplate,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
-  })
 
 }
+
+
+
+  
 
 exports.onCreateNode = ({ node, actions, getNode, createNodeId }) => {
   const { createNodeField } = actions
