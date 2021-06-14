2f319665c44ef952b08eca952d3f1574f59564ac
Jake Zien <jake@floored.com>
2021-06-14 00:19:54 -0400

    analytics cleanup

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -4,9 +4,7 @@ import Helmet from 'react-helmet'
 const Analytics = () => {
   return (
     <Helmet>
-      <script async defer data-host="https://protegant.io" data-dnt="false" src="https://protegant.io/js/script.js" id="ZwSg9rf6GA"></script>
       <script async data-goatcounter="https://jakezien.goatcounter.com/count" src="//gc.zgo.at/count.js"></script>
-      <script async src="https://app.jitt.io/pixel/I5EkPtLtiykovzTw"></script>
       <script async src={`/aap.js`}></script>
     </Helmet>
   )
